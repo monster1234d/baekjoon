b34bcb18cf8dc7eb6e606e00011b3bc5bc9b45cf
monster1234d <144297091+monster1234d@users.noreply.github.com>
2023-11-21 20:01:30 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 448 ms, Memory: 8924 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int compare(const void* x, const void* y)
+{
+	if (*(int*)x > *(int*)y)
+		return 1;
+	else if (*(int*)x < *(int*)y)
+		return -1;
+	else
+		return 0;
+}
+int arr[1000000] = { 0 };
+int main() 
+{
+	int a = 0;
+	int b = 0;
+
+	
+	scanf("%d", &a);
+	for (int i = 0; i < a; i++) 
+	{
+		scanf("%d", &arr[i]);
+	}
+	
+	qsort(arr, a, sizeof(int), compare);
+	for (int i = 0; i < a; i++) 
+	{
+		printf("%d\n", arr[i]);
+	}
+	return 0;
+}
